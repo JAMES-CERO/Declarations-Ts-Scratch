db81562ce608825330bda3ee3d2d4e4196da13f9
James Aules <james_aules@icloud.com>
2023-06-07 20:47:58 -0400

    declarations

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,11 @@
-declare module 'lodash' {}
+declare module 'lodash' {
+    function multiply(a: number, b:number): number
+
+    // the below is an example of function overloading
+    function reverse(arr: string[]):string[]
+    function reverse(arr: number[]):number[]
+    // using the generic below replaces the need for function overloading.
+
+    //union type can be used for an array with multiple types
+    function dropRight(arr: (string|number)[], dropped: number):(string|number)[]
+}
